8facc0c98b1ada733e1943f65bfab346608a0818
Ha Anna <its.haanna@gmail.com>
2023-02-09 16:09:51 +0900

    Update: Working eval for 1 wiki page

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const puppeteer = require('puppeteer')
 async function run() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://www.traversymedia.com')
+  await page.goto('https://en.wikipedia.org/wiki/Extraordinary_Attorney_Woo', {waitUntil : 'domcontentloaded'})
 
+  const links = await page.evaluate(() => Array.from(document.querySelectorAll('table.wikitable:nth-child(62) > tbody:nth-child(2) > tr'), (e) => e.textContent))
 
+  console.log(links[2])
 
   await browser.close()
 }
