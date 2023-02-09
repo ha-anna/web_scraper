574c2771de2e0ae2971756fa54cdc0f1b59df5ca
Ha Anna <its.haanna@gmail.com>
2023-02-09 14:54:02 +0900

    Upload: basic puppeteer setup

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const puppeteer = require('puppeteer')
 
 async function run() {
   const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.traversymedia.com')
+
+
+
+  await browser.close()
 }
 
 run()
